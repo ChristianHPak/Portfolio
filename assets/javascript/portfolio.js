3a199e0d33725fc90dbed544854cbef030574b15
Christian Pak <iamchristianpak@gmail.com>
2019-07-02 16:18:47 -0600

    finished filling in information for portfolio cards

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,13 @@
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
+// window.onload = function () {
+//     setTimeout(function () {
+//         PopUp('show');
+//     }, 1500);
+// }
+
+$("#popup_button").on("click", function() {
+    $("#ac-wrapper").css("display", "none");
+})
